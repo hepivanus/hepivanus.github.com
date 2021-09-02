addb6ec5b8ecb25ccb2f1c4610498c05bc916d3d
hepivanus <hepivanus@gmail.com>
2021-09-02 05:29:30 -0700

    commt

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const textElement = ["Web Developer, App Mobile Android & iOS, IT Support."];
+let count = 0;
+let txtIndex = 0;
+let currentTxt = 0;
+let words = "";
+(function ngetik() {
+  console.log(textElement[0]);
+});
